d595a8fa644cc276dade6c9187a6f7baffaf9631
Sensotrend <mikael@herfantasticnerd.com>
2015-11-13 13:49:38 +0200

    Styled hands
    Still not beautiful.

--- src/simple_analog.h
+++ src/simple_analog.h
@@ -3,17 +3,21 @@
 #include "pebble.h"
 
 static const GPathInfo MINUTE_HAND_POINTS = {
-  3, (GPoint []) {
-    { -8, 20 },
-    { 8, 20 },
-    { 0, -80 }
+  5, (GPoint []) {
+    { -3, 3 },
+    { 3, 3 },
+    { 2, -75 },
+    { 0, -80 },
+    { -2, -75 }
   }
 };
 
 static const GPathInfo HOUR_HAND_POINTS = {
-  3, (GPoint []){
-    {-6, 20},
-    {6, 20},
-    {0, -60}
+  5, (GPoint []) {
+    { -3, 3 },
+    { 3, 3 },
+    { 3, -35 },
+    { 0, -45 },
+    { -3, -35 }
   }
 };
